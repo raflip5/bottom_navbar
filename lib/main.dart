d928a6e012e7b025201f55cd7fc341c540c3310f
raflip5 <raflipahlevi2001.1911010129@mail.darmajaya.ac.id>
2022-10-19 22:34:38 +0700

    commit pertama

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:bottom_bar/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bottom Bar Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentPage = 0;
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: [
+          Container(color: Color.fromARGB(255, 4, 197, 227)),
+          Container(color: Color.fromARGB(255, 250, 245, 0)),
+          Container(color: Color.fromARGB(255, 10, 234, 103)),
+          Container(color: Color.fromARGB(255, 250, 101, 64)),
+        ],
+        onPageChanged: (index) {
+          setState(() => _currentPage = index);
+        },
+      ),
+      bottomNavigationBar: BottomBar(
+        textStyle: TextStyle(fontWeight: FontWeight.bold),
+        selectedIndex: _currentPage,
+        onTap: (int index) {
+          _pageController.jumpToPage(index);
+          setState(() => _currentPage = index);
+        },
+        items: <BottomBarItem>[
+          BottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+            activeColor: Colors.blue,
+            activeTitleColor: Colors.blue.shade600,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+            activeColor: Colors.red,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Account'),
+            backgroundColorOpacity: 0.1,
+            activeColor: Colors.greenAccent.shade700,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+            activeColor: Colors.orange,
+            activeIconColor: Colors.orange.shade600,
+            activeTitleColor: Colors.orange.shade700,
+          ),
+        ],
+      ),
+    );
+  }
+}
